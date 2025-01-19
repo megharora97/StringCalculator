a1bb088e7a0d3224af1518fa2d322b783d5f4754
Megha Arora <megharora@Meghas-Air.lan>
2025-01-20 01:24:58 +0530

    Solve test case: Handle custom delimiters

--- app/add.js
+++ app/add.js
@@ -1,9 +1,18 @@
 export default function add(numbers) {
   if (!numbers) return 0;
 
-  const delimiters = [",", "\n"];
+  let delimiters = [",", "\n"];
+  let numString = numbers;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    const customDelimiter = parts[0].slice(2);
+    delimiters.push(customDelimiter);
+    numString = parts.slice(1).join("\n");
+  }
+
   const regex = new RegExp(`[${delimiters.join("")}]`);
-  const numArray = numbers.split(regex).map(Number);
+  const numArray = numString.split(regex).map(Number);
 
   return numArray.reduce((sum, num) => sum + num, 0);
 }
